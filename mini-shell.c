c30f6f8f581afdd6554d69e7faa28a84e5adc51a
Jose Carlos <A01702828@itesm.mx>
2020-05-09 16:04:35 -0500

    primera validacion: Marcar error cuando no reciba argumentos

--- mini-shell.c
+++ mini-shell.c
@@ -4,7 +4,7 @@
  Author      : Jose Carlos Pacheco
  ID          : A01702828
  Version     : v1.0
- Description : Mini shell for Advance programming subject, requiriments on the PDF
+ Description : Mini shell para materia de programacion avanzada , requerimientos en el ./info/proyecto.pdf
  =======================================================================================================================
  */
 
@@ -17,12 +17,25 @@
 #include <sys/stat.h>
 
 
-// Methods
+// Metodos
 
 
 
-// Main
+/* Main
+ *
+ * Input: minimo 1 argumento maximo ?
+ * Input type: debera validar comandos existentes o un archivo .txt.bat.bin
+ *
+ * Output: La ejecucion de los comandos ingresados o en el caso la ejecucion de los comandos de archvio
+ *
+ */
 int main(int argc, char* argv[]) {
 
+    if( argc <= 1){
+        printf("usage: %s hacen falta argumentos\n", argv[0]);
+        return -1;
+    }
+
+    exit(0);
 
 }
