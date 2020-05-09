d2efe26c2a47aaa324c697bc33e99fe142e9ae5c
Jose Carlos <A01702828@itesm.mx>
2020-05-09 15:46:29 -0500

    Starting commit with the initial documents

---
+++ mini-shell.c
@@ -0,0 +1,28 @@
+/*
+ =======================================================================================================================
+ Name        : mini-shell.c
+ Author      : Jose Carlos Pacheco
+ ID          : A01702828
+ Version     : v1.0
+ Description : Mini shell for Advance programming subject, requiriments on the PDF
+ =======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+
+// Methods
+
+
+
+// Main
+int main(int argc, char* argv[]) {
+
+
+}
